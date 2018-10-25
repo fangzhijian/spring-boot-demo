73d57437e6ad45bdcc674c46651e9f1e1df578c5
fangzhijian <657833870@qq.com>
2018-10-25 16:07:41 +0800

    这是谁啊

--- src/main/java/com/example/boot/Test.java
+++ src/main/java/com/example/boot/Test.java
@@ -12,6 +12,6 @@ public class Test {
 
         Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.0.34:3306/test?characterEncoding=UTF-8&allowMultiQueries=true&useSSL=false","root","root");
         System.out.println(connection.isReadOnly());
-        System.out.println("我爱你老婆");
+        System.out.println(" 我爱你老婆");
     }
 }
